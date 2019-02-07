a8a45db4beb4f49b128bbb5b8e4bc6460e9125a9
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-02-07 15:57:57 -0500

    pre-lint pre Webpack

---
+++ public/assets/js/additional.js
@@ -0,0 +1,10 @@
+function changeTitle(text) {
+    document.title = "test";
+}
+
+document.getElementById("nodeGoto").addEventListener("click", function() {
+    gotoNode(result.name);
+}, false);
+
+
+// $(document).find("title").text();
